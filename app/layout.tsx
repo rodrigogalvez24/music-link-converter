cd8edeef01e061a13ee69e8426f0fa9050e2afcc
rodrigogalvez24 <rodrigogalvezglz@gmail.com>
2025-07-03 11:18:25 -0600

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es">
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
