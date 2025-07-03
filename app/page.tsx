d1275c19a725cb2205a68e98b7b72c692df01252
rodrigogalvez24 <rodrigogalvezglz@gmail.com>
2025-07-03 11:28:56 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 'use client'
 import { useState } from "react";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "../components/ui/input";
+import { Button } from "../components/ui/button";
+import { Card, CardContent } from "../components/ui/card";
+
 
 export default function Home() {
   const [inputUrl, setInputUrl] = useState("");
